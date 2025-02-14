3b7a6f51823a7a55c26345a3b012e7233dd8c653
CalmFarm <vhzpxj@gmail.com>
2025-02-15 02:08:38 +0900

    Ability Activation Policy

---
+++ Source/ARPG/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -0,0 +1,32 @@
+// HS
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "WarriorGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EWarriorAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	// UGameplayAbility Interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	// UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, Category="WarriorAbility")
+	EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
+};
---
+++ Source/ARPG/Public/Items/Weapons/WarriorWeaponBase.h
@@ -0,0 +1,26 @@
+// HS
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WarriorWeaponBase.generated.h"
+
+UCLASS()
+class ARPG_API AWarriorWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWarriorWeaponBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
