6f6c4beb8fbd865e2db9c0f935a28c50f9af86f9
CalmFarm <vhzpxj@gmail.com>
2025-02-16 21:15:58 +0900

    Gameplay Event AnimNotify

--- Source/ARPG/Private/WarriorGameplayTags.cpp
+++ Source/ARPG/Private/WarriorGameplayTags.cpp
@@ -11,4 +11,7 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnEquip,"InputTag.UnEquip");
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe, "Player.Event.Equip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_UnEquip_Axe, "Player.Event.UnEquip.Axe");
 }
--- Source/ARPG/Public/WarriorGameplayTags.h
+++ Source/ARPG/Public/WarriorGameplayTags.h
@@ -14,4 +14,7 @@ namespace WarriorGameplayTags
 
 	// Player Tags
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Weapon_Axe);
+
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_UnEquip_Axe);
 }
