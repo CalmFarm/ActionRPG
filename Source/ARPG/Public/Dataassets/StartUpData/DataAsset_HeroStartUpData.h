63b0473d2f438093b10f0bfc46bb19506f311c81
CalmFarm <vhzpxj@gmail.com>
2025-02-16 20:23:20 +0900

    Hero Ability Set

--- Source/ARPG/Public/Dataassets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/ARPG/Public/Dataassets/StartUpData/DataAsset_HeroStartUpData.h
@@ -3,9 +3,23 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Dataassets/StartUpData/DataAsset_StartUpDataBase.h"
 #include "DataAsset_HeroStartUpData.generated.h"
 
+USTRUCT(BlueprintType)
+struct FWarriorHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(category="InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+};
 /**
  * 
  */
@@ -13,5 +27,10 @@ UCLASS()
 class ARPG_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta=(TitleProperty="InputTag"))
+	TArray<FWarriorHeroAbilitySet> HeroStartUpAbilitySet;
 };
