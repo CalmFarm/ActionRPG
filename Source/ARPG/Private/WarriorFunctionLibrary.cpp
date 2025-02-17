b67f660bff2aee3d8f2e63cec3b278bd071f3b0c
CalmFarm <vhzpxj@gmail.com>
2025-02-17 22:26:20 +0900

    Warrior funtion library

---
+++ Source/ARPG/Private/WarriorFunctionLibrary.cpp
@@ -0,0 +1,48 @@
+// HS
+
+
+#include "WarriorFunctionLibrary.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+UWarriorAbilitySystemComponent* UWarriorFunctionLibrary::NativeGetWarriorAscFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	return CastChecked<UWarriorAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor)); 
+}
+
+void UWarriorFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorAscFromActor(InActor);
+
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UWarriorFunctionLibrary::RemoveGameplayFromActorIfFound(AActor* InActor, FGameplayTag RemoveToAdd)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorAscFromActor(InActor);
+
+	if (!ASC->HasMatchingGameplayTag(RemoveToAdd))
+	{
+		ASC->AddLooseGameplayTag(RemoveToAdd);
+	}
+}
+
+
+bool UWarriorFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorAscFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UWarriorFunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck,
+	EWarriorConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, TagToCheck)?  EWarriorConfirmType::Yes : EWarriorConfirmType::No;
+}
