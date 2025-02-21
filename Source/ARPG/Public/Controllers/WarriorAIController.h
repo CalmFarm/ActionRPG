a805df5ec6865228cd0fe821e95ca3629e7e22bf
CalmFarm <vhzpxj@gmail.com>
2025-02-22 00:22:02 +0900

    AI Avoidance SetUp

--- Source/ARPG/Public/Controllers/WarriorAIController.h
+++ Source/ARPG/Public/Controllers/WarriorAIController.h
@@ -6,6 +6,7 @@
 #include "AIController.h"
 #include "WarriorAIController.generated.h"
 
+struct FAIStimulus;
 class UAISenseConfig_Sight;
 class UAIPerceptionComponent;
 /**
@@ -24,6 +25,8 @@ public:
 	// IGenericTeamAgentInterface
 	
 protected:
+	virtual void BeginPlay() override;
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UAIPerceptionComponent* EnemyPerceptionComponent;
 
@@ -32,4 +35,14 @@ protected:
 
 	UFUNCTION()
 	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Detour Crowd Avoidance Config")
+	bool bEnableDetourCrowdAvoidance = true;
+
+	UPROPERTY(EditDefaultsOnly, Category="Detour Crowd Avoidance Config", meta=(EditCondition = "bEnableDetourCrowdAvoidance"), meta=(UIMin = "1", UIMax = "4"))
+	int32 DetourCrowdAvoidanceQuality = 4;
+
+	UPROPERTY(EditDefaultsOnly, Category="Detour Crowd Avoidance Config", meta=(EditCondition = "bEnableDetourCrowdAvoidance"))
+	float CollisionQueryRange = 600.f;
 };
