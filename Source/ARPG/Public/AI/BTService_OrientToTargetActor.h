1d65decd9cc63377fbfc429574d6943f5011d1f4
CalmFarm <vhzpxj@gmail.com>
2025-02-22 19:05:55 +0900

    orient to targetactor

---
+++ Source/ARPG/Public/AI/BTService_OrientToTargetActor.h
@@ -0,0 +1,31 @@
+// HS
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_OrientToTargetActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UBTService_OrientToTargetActor : public UBTService
+{
+	GENERATED_BODY()
+
+	UBTService_OrientToTargetActor();
+
+	//~ Begin UBTNode Interface
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual FString GetStaticDescription() const override;
+	//~ End UBTNode Interface
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+	UPROPERTY(EditAnywhere,Category="Target")
+	FBlackboardKeySelector InTargetActorKey;
+
+	UPROPERTY(EditAnywhere,Category="Target")
+	float RotationInterpSpeed;
+};
