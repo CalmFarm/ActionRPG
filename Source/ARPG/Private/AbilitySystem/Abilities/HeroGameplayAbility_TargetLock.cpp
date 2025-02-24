6b2789c74468448a74e109304efcbaa840cb67eb
CalmFarm <vhzpxj@gmail.com>
2025-02-24 22:04:52 +0900

    FindNearestActor

--- Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -5,6 +5,7 @@
 
 #include "WarriorDebugHelper.h"
 #include "Characters/WarriorHeroCharacter.h"
+#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
@@ -20,12 +21,31 @@ void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandl
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
+	CleanUp();
+	
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
 void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
 {
 	GetAvailableActorsToLock();
+
+	if (AvailableActorsToLock.IsEmpty())
+	{
+		CancelTargetLockAbility();
+		return;
+	}
+
+	CurrentLockedActor = GetNearestTargetFromAvailableActors(AvailableActorsToLock);
+
+	if (CurrentLockedActor)
+	{
+		Debug::Print(CurrentLockedActor->GetActorNameOrLabel());
+	}
+	else
+	{
+		CancelTargetLockAbility();
+	}
 }
 
 void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
@@ -53,9 +73,25 @@ void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
 			if (HitActor != GetHeroCharacterFromActorInfo())
 			{
 				AvailableActorsToLock.AddUnique(HitActor);
-
-				Debug::Print(HitActor->GetActorNameOrLabel());
 			}
 		}
 	}
+}
+
+AActor* UHeroGameplayAbility_TargetLock::GetNearestTargetFromAvailableActors(const TArray<AActor*>& InAvailableActors)
+{
+	float ClosestDistance = 0.f;
+	return UGameplayStatics::FindNearestActor(GetHeroCharacterFromActorInfo()->GetActorLocation(), InAvailableActors, ClosestDistance);
+}
+
+void UHeroGameplayAbility_TargetLock::CancelTargetLockAbility()
+{
+	CancelAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true);
+}
+
+void UHeroGameplayAbility_TargetLock::CleanUp()
+{
+	AvailableActorsToLock.Empty();
+
+	CurrentLockedActor = nullptr;
 }
