244415d7edc4b81a7bfa4bc5af9b10127d270ea5
CalmFarm <vhzpxj@gmail.com>
2025-02-25 20:07:47 +0900

    Target Locking Aim Enemy

--- Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -14,6 +14,7 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Widgets/WarriorWidgetBase.h"
 #include "Controllers/WarriorHeroController.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                                       const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
@@ -43,6 +44,23 @@ void UHeroGameplayAbility_TargetLock::OnTargetLockTick(float DeltaTime)
 	}
 
 	SetTargetLockWidgetPosition();
+
+	const bool bShouldOverrideRotation =
+		!UWarriorFunctionLibrary::NativeDoesActorHaveTag(GetHeroCharacterFromActorInfo(), WarriorGameplayTags::Player_Status_Rolling) &&
+			!UWarriorFunctionLibrary::NativeDoesActorHaveTag(GetHeroCharacterFromActorInfo(), WarriorGameplayTags::Player_Status_Blocking);
+
+	if (bShouldOverrideRotation)
+	{
+		const FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(
+			GetHeroCharacterFromActorInfo()->GetActorLocation(),
+			CurrentLockedActor->GetActorLocation());
+
+		const FRotator CurrentControlRot = GetHeroControllerFromActorInfo()->GetControlRotation();
+		const FRotator TargetRot = FMath::RInterpTo(CurrentControlRot, LookAtRot, DeltaTime, TargetLockRotationInterpSpeed);
+
+		GetHeroControllerFromActorInfo()->SetControlRotation(FRotator(TargetRot.Pitch, TargetRot.Yaw, 0.f));
+		GetHeroCharacterFromActorInfo()->SetActorRotation(FRotator(0.f, TargetRot.Yaw, 0.f));
+	}
 }
 
 void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
--- Source/ARPG/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/ARPG/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -48,7 +48,10 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category="Target Lock")
 	TSubclassOf<UWarriorWidgetBase> TargetLockWidgetClass;
-
+	
+	UPROPERTY(EditDefaultsOnly, Category="Target Lock")
+	float TargetLockRotationInterpSpeed = 5.f;
+	
 	UPROPERTY()
 	TArray<AActor*> AvailableActorsToLock;
 
