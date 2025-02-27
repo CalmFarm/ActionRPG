878fdb1d0d77b98c87f4d71c80d5230048b9b7af
CalmFarm <vhzpxj@gmail.com>
2025-02-27 17:18:57 +0900

    Create Boss Enemy && Remove Bossbar

--- Source/ARPG/Private/Components/UI/EnemyPawnUIComponent.cpp
+++ Source/ARPG/Private/Components/UI/EnemyPawnUIComponent.cpp
@@ -3,3 +3,25 @@
 
 #include "Components/UI/EnemyPawnUIComponent.h"
 
+#include "Widgets/WarriorWidgetBase.h"
+
+void UEnemyPawnUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyPawnUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+	if (EnemyDrawnWidgets.IsEmpty())
+	{
+		return;
+	}
+
+	for (UWarriorWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+	{
+		if (DrawnWidget)
+		{
+			DrawnWidget->RemoveFromParent();
+		}
+	}
+}
