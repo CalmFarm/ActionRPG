24c52da20aec70f32de43a7fd504b344b2f856ed
CalmFarm <vhzpxj@gmail.com>
2025-03-03 22:53:42 +0900

    Collect Stones

--- Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.cpp
+++ Source/ARPG/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.cpp
@@ -3,9 +3,13 @@
 
 #include "AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h"
 
+#include "Characters/WarriorHeroCharacter.h"
+#include "Items/Pickups/WarriorStoneBase.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 void UHeroGameplayAbility_PickUpStones::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+                                                        const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                        const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
@@ -15,4 +19,36 @@ void UHeroGameplayAbility_PickUpStones::EndAbility(const FGameplayAbilitySpecHan
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_PickUpStones::CollectStones()
+{
+	CollectedStone.Empty();
+	
+	TArray<FHitResult> HitResults;
+	
+	UKismetSystemLibrary::BoxTraceMultiForObjects(GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + -GetHeroCharacterFromActorInfo()->GetActorUpVector() * BoxTraceDistance,
+		TraceBoxSize / 2,
+		(-GetHeroCharacterFromActorInfo()->GetActorUpVector()).ToOrientationRotator(),
+		StoneTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bDrawDebugShape? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		HitResults,
+		true);
+
+	for (const FHitResult& TraceHit : HitResults)
+	{
+		if (AWarriorStoneBase* FoundStone = Cast<AWarriorStoneBase>(TraceHit.GetActor()))
+		{
+			CollectedStone.AddUnique(FoundStone);
+		}
+	}
+
+	if (CollectedStone.IsEmpty())
+	{
+		CancelAbility(GetCurrentAbilitySpecHandle(),GetCurrentActorInfo(),GetCurrentActivationInfo(),true);
+	}
 }
--- Source/ARPG/Public/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h
+++ Source/ARPG/Public/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h
@@ -6,6 +6,7 @@
 #include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
 #include "HeroGameplayAbility_PickUpStones.generated.h"
 
+class AWarriorStoneBase;
 /**
  * 
  */
@@ -15,6 +16,27 @@ class ARPG_API UHeroGameplayAbility_PickUpStones : public UWarriorHeroGameplayAb
 	GENERATED_BODY()
 
 protected:
+	//~Begin GameplayAbility
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~End GameplayAbility
+
+	UFUNCTION(BlueprintCallable)
+	void CollectStones();
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float BoxTraceDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector TraceBoxSize = FVector(100.f);
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TEnumAsByte<	EObjectTypeQuery> > StoneTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool bDrawDebugShape = false;
+
+	UPROPERTY()
+	TArray<AWarriorStoneBase*> CollectedStone;
 };
