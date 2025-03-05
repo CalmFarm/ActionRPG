25c47c08f37604d76c38010cae38e8a523a08ebf
CalmFarm <vhzpxj@gmail.com>
2025-03-05 21:00:17 +0900

    Wave Spawn Logic

--- Source/ARPG/Private/GameModes/WarriorSurvivorGameMode.cpp
+++ Source/ARPG/Private/GameModes/WarriorSurvivorGameMode.cpp
@@ -6,11 +6,64 @@
 void AWarriorSurvivorGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	checkf(EnemyWaveSpawnerDataTable, TEXT("Forgot to assign a valid data table in survivor game mode blueprint"));
+
+	SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState::WaitSpawnNewWave);
+
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable->GetRowNames().Num();
 }
 
 void AWarriorSurvivorGameMode::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	if (CurrentSurvivorGameModeState == EWarriorSurvivorGameModeState::WaitSpawnNewWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+		
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState::SpawningWave);
+		}
+	}
+
+	if (CurrentSurvivorGameModeState == EWarriorSurvivorGameModeState::SpawningWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= SpawnEnemiesDelayTime)
+		{
+			//TODO : Handle New Spawn Enemy
+
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState::InProgress);
+		}
+	}
+
+	if (CurrentSurvivorGameModeState == EWarriorSurvivorGameModeState::WaveComplete)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= WaveCompletedWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			CurrentWaveCount++;
+
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState::AllWavesDone);
+			}
+			else
+			{
+				SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState::WaitSpawnNewWave);
+			}
+		}
+	}
 }
 
 void AWarriorSurvivorGameMode::SetCurrentSurvivorGameModeState(EWarriorSurvivorGameModeState InState)
@@ -19,3 +72,9 @@ void AWarriorSurvivorGameMode::SetCurrentSurvivorGameModeState(EWarriorSurvivorG
 
 	OnSurvivorGameModeStateChanged.Broadcast(CurrentSurvivorGameModeState);
 }
+
+bool AWarriorSurvivorGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount >= TotalWavesToSpawn;
+}
+
